7a6fe0c622c092f5377aaac4e15aed4a27a1ec1b
Emmanuel <enmanuelmartinez@yahoo.es>
2023-12-01 21:56:07 -0600

    modify environment

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
   baseUrl: 'https://www.omdbapi.com/',//'https://ott-details.p.rapidapi.com/search'
-  baseUrlDetails: 'https://www.omdbapi.com/',//'http://www.omdbapi.com/?i=tt0115392&type=series&apikey=abe106a9',
+  baseUrlDetails: 'http://www.omdbapi.com/',//'http://www.omdbapi.com/?i=tt0115392&type=series&apikey=abe106a9',
   apiKey:'abe106a9'
 }
