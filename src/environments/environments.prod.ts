246b925f92d31f7011d2af0a5941bbe2a2574866
Emmanuel <enmanuelmartinez@yahoo.es>
2023-12-01 21:45:59 -0600

    add folder build https

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments = {
-  baseUrl: 'http://www.omdbapi.com/',//'https://ott-details.p.rapidapi.com/search'
-  baseUrlDetails: 'http://www.omdbapi.com/',//'http://www.omdbapi.com/?i=tt0115392&type=series&apikey=abe106a9',
+  baseUrl: 'https://www.omdbapi.com/',//'https://ott-details.p.rapidapi.com/search'
+  baseUrlDetails: 'https://www.omdbapi.com/',//'http://www.omdbapi.com/?i=tt0115392&type=series&apikey=abe106a9',
   apiKey:'abe106a9'
 }
